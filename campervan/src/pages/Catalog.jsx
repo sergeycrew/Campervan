41a564a696b2875536ed2f956c4178892b4b267d
Sergey Crew <sergeycrew.bs@gmail.com>
2024-03-21 19:17:32 -0700

    filters

---
+++ campervan/src/pages/Catalog.jsx
@@ -0,0 +1,10 @@
+import { Container } from "../styles/container";
+import { Filter } from "components/Filter/Filter";
+
+export const Catalog = () => {
+  return (
+    <Container>
+      <Filter />
+    </Container>
+  );
+};
